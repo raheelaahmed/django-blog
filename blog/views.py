c19deb1c85b93548e160d0e577afbd8808360f8b
Raheela Aftab <raheela.aftab@gmail.com>
2024-10-08 15:45:27 +0000

    add and delete comments

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
-from .models import Post
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from .models import Post, Comment
+from .forms import CommentForm
 
-# Create your views here.
+# Create your views here.   
+
 
 
 class PostList(generic.ListView):
@@ -21,14 +25,67 @@ def post_detail(request, slug):
 
     **Template:**
 
-    :template:`blog/post_detail.html`
+    :template:`blog/post_detail.html`   
+
     """
 
-    queryset = Post.objects.filter(status=1)
-    post = get_object_or_404(queryset, slug=slug)
+    post = get_object_or_404(Post.objects.filter(status=1), slug=slug)
+    comments = post.comments.all().order_by("-created_on")
+    comment_count = post.comments.filter(approved=True).count()
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+    if comment_form.is_valid():
+        comment = comment_form.save(commit=False)
+        comment.author = request.user
+        comment.post = post
+        comment.save()
+        messages.add_message(
+        request, messages.SUCCESS,
+        'Comment submitted and awaiting approval'
+    )
+    comment_form = CommentForm()
 
     return render(
         request,
         "blog/post_detail.html",
-        {"post": post, "coder": "Raheela Aftab"},
+        {
+            "post": post,
+            "comments": comments,
+            "comment_count": comment_count,
+             "comment_form": comment_form,
+        },
     )
+
+from django.shortcuts import render, redirect
+from .models import Comment
+
+def comment_edit(request, comment_id):
+    comment = get_object_or_404(Comment, pk=comment_id)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            form.save()
+            return redirect('post_detail', slug=comment.post.slug)
+    else:
+        form = CommentForm(instance=comment)
+
+    return render(request, 'blog/comment_edit.html', {'form': form, 'comment': comment})
+
+
+
+def comment_delete(request, slug, comment_id):
+    """
+    view to delete comment
+    """
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    comment = get_object_or_404(Comment, pk=comment_id)
+
+    if comment.author == request.user:
+        comment.delete()
+        messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
+    else:
+        messages.add_message(request, messages.ERROR, 'You can only delete your own comments!')
+
+    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
